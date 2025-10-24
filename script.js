be5f647b23b3dd114ad52d0ec66f5a58089413ea
07alizaa <simkhadaaliza080@gmail.com>
2025-10-24 17:59:31 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ document.getElementById('loginForm').addEventListener('submit', function (event) {
+      let username = document.getElementById('username').value;
+      let password = document.getElementById('password').value;
+
+      if (username === '' || password === '') {
+        alert("Please fill in both fields.");
+        event.preventDefault(); // stop form submission
+      } else {
+        alert("Login successful!");
+      }
+    });
